c70d86fce30dcc8228e8b4a83044c3ec1df7656f
Ansel Chang <31443946+AnselChang@users.noreply.github.com>
2022-07-31 14:05:36 -0400

    refactor

--- server.js
+++ server.js
@@ -9,12 +9,18 @@ let syncnum1 = 0;
 let syncnum2 = 0;
 let syncnum3 = 0;
 
+var values = {
+  "red" : 0,
+  "blue" : 0,
+  "mid" : 0
+};
+
 app.get('/', (req,res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
 function emit() {
-  io.emit("server broadcast", syncnum1, syncnum2, syncnum3);
+  io.emit("server broadcast", values);
 }
 
 io.on('connection', (socket) => {
@@ -26,35 +32,11 @@ io.on('connection', (socket) => {
         console.log('user disconnected');
     });
 
-    socket.on('inc num1', () => {
-        ++syncnum1;
+    socket.on('button', (button, delta) => {
+        values[button] += delta;
         emit();
     });
 
-    socket.on('dec num1', () => {
-        --syncnum1;
-        emit();
-    });
-
-    socket.on('inc num2', () => {
-        ++syncnum2;
-        emit();
-    });
-
-    socket.on('dec num2', () => {
-        --syncnum2;
-        emit();
-    });
-
-    socket.on('inc num3', () => {
-        ++syncnum3;
-        emit();
-    });
-
-    socket.on('dec num3', () => {
-        --syncnum3;
-        emit();
-    });
 });
 
 http.listen(port, () => {
