8338e8f120c22b9fcaf19105dcfa53c7e588ac89
TJ Nickerson <tjpnick@gmail.com>
2022-08-01 18:53:31 -0400

    less less brainrot

--- script.js
+++ script.js
@@ -17,20 +17,10 @@ function updateHTML() {
     }
     document.getElementById('yelvalue').innerHTML = clientValues[side]["yellow"] + "<br>(+)";
     document.getElementById('penvalue').innerHTML = clientValues[side]["penalty"] + "<br>(+)";
-
-    if (clientValues[side]["auton"] === 0) {
-        document.getElementById('a1').style.backgroundColor = "#F2881F";
-        document.getElementById('a2').style.backgroundColor = "#FBB269";
-        document.getElementById('a3').style.backgroundColor = "#FBB269";
-    } else if (clientValues[side]["auton"] === 1) {
-        document.getElementById('a1').style.backgroundColor = "#FBB269";
-        document.getElementById('a2').style.backgroundColor = "#F2881F";
-        document.getElementById('a3').style.backgroundColor = "#FBB269";
-    } else {
-        document.getElementById('a1').style.backgroundColor = "#FBB269";
-        document.getElementById('a2').style.backgroundColor = "#FBB269";
-        document.getElementById('a3').style.backgroundColor = "#F2881F";
+    for(let i = 1; i<=3; i++){
+        document.getElementById('a'+i).style.backgroundColor = "#FBB269";
     }
+    document.getElementById('a'+(clientValues[side]["auton"]+1)).style.backgroundColor = "#F2881F";
 
 }
 function resetScoring(){
