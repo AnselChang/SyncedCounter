ad79820ff3d3d6bd6eb125933413e95a475d6241
Ansel Chang <31443946+AnselChang@users.noreply.github.com>
2022-08-01 10:12:44 -0400

    total score

--- server.js
+++ server.js
@@ -4,35 +4,46 @@ var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
 
 const clamp = (num, min, max) => Math.min(Math.max(num, min), max);
+const RED = 0;
+const BLUE = 1;
 
 console.log("start");
 
-var values = {
-  "REDSIDE" : {
+var values = [
+  {
     "red" : 0,
     "blue" : 0,
     "mid" : 0,
     "yellow" : 1,
     "ramp" : 0,
     "penalty" : 0,
-    "auton" : 1
+    "auton" : 0,
+    "score" : 0
   },
-  "BLUESIDE" : {
+  {
     "red" : 0,
     "blue" : 0,
     "mid" : 0,
     "yellow" : 1,
     "ramp" : 0,
     "penalty" : 0,
-    "auton" : 1
+    "auton" : 0,
+    "score" : 0
   }
-};
+];
 
 app.get('/', (req,res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
+function calcScore(team) {
+  values[team]["score"] = values[team]["red"]*5 + values[team]["blue"]*3 + values[team]["mid"]*5 + values[team]["yellow"]*10;
+  values[team]["score"] += values[team]["ramp"]*15 + values[team]["auton"]*5 - values[team]["penalty"];
+}
+
 function emit() {
+  calcScore(RED);
+  calcScore(BLUE);
   io.emit("server broadcast", values);
 }
 
@@ -45,12 +56,13 @@ io.on('connection', (socket) => {
         console.log('user disconnected');
     });
 
-    socket.on('button', (button, side, delta) => {
-      console.log("button", button, side, delta);
-      console.log(values["REDSIDE"]);
-        if (button === "yellow" || button === "ramp") values[side][button] = clamp(values[side][button], 0, 4);
-        else values[side][button] = Math.max(0, values[side][button] + delta);
-        emit();
+    socket.on('button', (side, button, delta) => {
+      console.log("button", side, button, delta);
+      console.log(values);
+      let newValue = values[side][button] + delta;
+      if (button === "yellow" || button === "ramp") values[side][button] = clamp(newValue, 0, 4);
+      else values[side][button] = Math.max(0, newValue);
+      emit();
     });
 
     socket.on('auton', (side, mode) => {
